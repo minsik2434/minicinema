b045114b9da2ac29ce6f87214ce88f4104387d73
minsik2434 <minsik2434@gmail.com>
2024-03-14 22:26:28 +0900

    푸시 테스트

--- mini/src/main/java/com/cinema/mini/controller/MovieController.java
+++ mini/src/main/java/com/cinema/mini/controller/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/movie")
 public class MovieController {
+    //movieController
     @GetMapping("/search")
     public String search(){
         return "view/search";
