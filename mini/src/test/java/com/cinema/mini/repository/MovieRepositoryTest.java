bf5921aae6dc1c9257645e5d211e07d337a49aa6
minsik2434 <minsik2434@gmail.com>
2024-03-20 18:01:58 +0900

    MovieRepository 테스트 수정

--- mini/src/test/java/com/cinema/mini/repository/MovieRepositoryTest.java
+++ mini/src/test/java/com/cinema/mini/repository/MovieRepositoryTest.java
@@ -9,11 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -65,29 +67,56 @@ public class MovieRepositoryTest {
 
     @Test
     void findRomanceMovieList(){
+        List<Movie> RomanceMovieList = new ArrayList<>();
+        Movie movie1 = createMovieAndMovieGenre("test01",
+                "테스트1", "2024-03-01",4,new String[] {"액션","로맨스"});
+        Movie movie2 = createMovieAndMovieGenre("test02",
+                "테스트2", "2024-03-01",4,new String[] {"가족","로맨스"});
+        Movie movie3 = createMovieAndMovieGenre("test03",
+                "테스트3", "2024-03-01",4,new String[] {"가족","액션"});
+
+        RomanceMovieList.add(movie1);
+        RomanceMovieList.add(movie2);
+
         Pageable pageable = PageRequest.of(0,5);
         List<Movie> list = movieRepository.findByMovieGenres_Genre_GenreName("로맨스", pageable);
+        assertThat(list).hasSize(2);
         Genre genre = genreRepository.findByGenreName("로맨스").orElseThrow();
+
         list.forEach(movie -> {
             boolean containsRomance = movie.getMovieGenres().stream()
                     .anyMatch(movieGenre -> movieGenre.getGenre().equals(genre));
             assertThat(containsRomance).isTrue();
         });
-    }
 
-    @Test
-    void findByTitleIsLikeTest(){
-        List<Movie> list = movieRepository.findByTitleContaining("가");
-        List<String> nameList = new ArrayList<>();
-        nameList.add("가여운 것들");
-        nameList.add("아가일");
         list.forEach(movie -> {
-            boolean containMovie = nameList.contains(movie.getTitle());
-            assertThat(containMovie).isTrue();
+            boolean isRomance = RomanceMovieList.contains(movie);
+            assertThat(isRomance).isTrue();
         });
     }
 
+
+    @Test
+    void findByTitleIsLikeTest(){
+        List<Movie> movieList = new ArrayList<>();
+        movieList.add(createTestMovie("test01", "테스트1", "2024-03-01", 4.5));
+        movieList.add(createTestMovie("test02", "테스트2", "2024-03-01", 4.5));
+        movieList.add(createTestMovie("test03", "테스트3", "2024-03-01", 4.5));
+        movieList.add(createTestMovie("test04", "abc", "2024-03-01", 4.5));
+        movieRepository.saveAll(movieList);
+
+
+        List<Movie> list = movieRepository.findByTitleContaining("테스트");
+        assertThat(list).hasSize(3);
+        for (Movie movie : list) {
+            assertThat(movie.getTitle()).contains("테스트");
+        }
+    }
+
+
+
     private Movie createTestMovie(String movieId, String movieTitle, String localDate, double grade) {
+        List<MovieGenre> list = new ArrayList<>();
         return Movie.builder()
                 .movieId(movieId)
                 .title(movieTitle)
@@ -95,7 +124,22 @@ public class MovieRepositoryTest {
                 .posterPath("testPosterPath")
                 .releaseDate(LocalDate.parse(localDate))
                 .overview("테스트 영화")
-                .grade(grade).build();
+                .grade(grade)
+                .movieGenres(list).build();
+    }
+
+    private Movie createMovieAndMovieGenre(String movieId, String title, String releaseDate, double grade, String[] genreNames) {
+        Movie movie = createTestMovie(movieId, title, releaseDate, grade);
+        for (String genreName : genreNames) {
+            MovieGenre movieGenre = getMovieGenre(genreName, movie);
+            movie.getMovieGenres().add(movieGenre);
+        }
+        return movieRepository.save(movie);
+    }
+
+    private MovieGenre getMovieGenre(String genreName, Movie movie) {
+        Genre genre = genreRepository.findByGenreName(genreName).orElseThrow();
+        return MovieGenre.builder().movie(movie).genre(genre).build();
     }
 }
 
