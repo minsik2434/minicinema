66e7ba1b1b00cb65f8e78ea799b1af6fe0eeaf67
minsik2434 <minsik2434@gmail.com>
2024-03-27 22:34:31 +0900

    ScreeningRepository, TheaterRepository 추가

--- mini/src/main/java/com/cinema/mini/repository/MovieRepository.java
+++ mini/src/main/java/com/cinema/mini/repository/MovieRepository.java
@@ -4,9 +4,11 @@ import com.cinema.mini.domain.Movie;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MovieRepository extends JpaRepository<Movie, String> {
     @Query("select m from Movie m order by m.releaseDate DESC limit 5")
     List<Movie> findLastestMovieList();
--- mini/src/main/java/com/cinema/mini/repository/TheaterRepository.java
+++ mini/src/main/java/com/cinema/mini/repository/TheaterRepository.java
@@ -2,6 +2,8 @@ package com.cinema.mini.repository;
 
 import com.cinema.mini.domain.Theater;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
 }
